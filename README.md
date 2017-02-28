b11e9cbcd484cf35e46866d5de5a8b494935def4
heavenchou <heaven.chou@gmail.com>
2017-02-28 17:16:52 +0800

    第一次提交什麼都沒有的基本視窗

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CBReader
 CBETA CBReader 2X
+
+Project created by Heaven Chou in Taiwan at 2/28,2017
---
+++ src/main.h
@@ -0,0 +1,25 @@
+//---------------------------------------------------------------------------
+
+#ifndef mainH
+#define mainH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TfmMain : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TLabel *Label1;
+private:	// User declarations
+public:		// User declarations
+	__fastcall TfmMain(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TfmMain *fmMain;
+//---------------------------------------------------------------------------
+#endif
