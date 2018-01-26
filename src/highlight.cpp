6a9f61eb740ebf9503a77f70c97e3000ec5d1c7c
heavenchou <heaven.chou@gmail.com>
2018-01-26 16:45:15 +0800

    處理缺字塗色

--- src/highlight.cpp
+++ src/highlight.cpp
@@ -135,7 +135,15 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 	int iPosTmp;
 	wchar_t * pPoint = HTMLSource->FirstChar();		// 遊走在 HTMLSource 的指標
 	wchar_t * pPointTmp;
+
+	// 處理缺字用的
+	wchar_t * pDesPoint = 0;
+	wchar_t * pUniPoint = 0;
+	int iDesLen = 0;
+	int iUniLen = 0;
+
 	wchar_t * pFindWord = sWord.c_str();		// 要塗色的字
+	pFindWord = NextFindPoint(pFindWord); 	// 移到下一個可以查詢的字
 
 	// 例如搜尋 "雜阿含 + 阿含" , 這個字串會出現在 HTML 最上方.
 	// 第二組的 "阿含" 必須在第一組之後, 以免重疊
@@ -149,7 +157,7 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 		iAfterThisPos = tpPtr->y;
     }
 
-	// 先移至 <!-- search head --> 的地方
+	// 先移至 <div id="SearchHead"> 的地方
 
 	while(*pPoint)
 	{
@@ -165,6 +173,9 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 	{
         ShowMessage(u"沒找到指定標記，程式要再檢查處理");
     }
+
+    // =====================================================
+	// 開始分析
 
 	while(*pPoint)
 	{
@@ -202,6 +213,12 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 				continue;
 			}
 
+			// 處理缺字
+			if (wcsncmp(pPoint, L"<span class=\"gaiji\"", 19) == 0)
+			{
+				AnalysisGiajiTag(&pPoint, &pDesPoint, &pUniPoint, &iDesLen, &iUniLen); // 處理缺字標記
+			}
+
 			// 1.遇到標記, 要額外處理
 
 			if(*pPoint == u'<')
@@ -224,30 +241,14 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 				continue;
 			}
 
-			// 2.忽略標點符號
-
-			if(CWordTool::GetWordType(*pPoint) == CWordTool::wtPunc)
-			{
-				pPoint++;
-				continue;
-			}
-
-			// 3.忽略空白
-
-			if(CWordTool::GetWordType(*pPoint) == CWordTool::wtSpace)
-			{
-				pPoint++;
-				continue;
-			}
-
-			// 4.星號 [＊]
+			// 2.星號 [＊]
 			if (wcsncmp(pPoint, L"[＊]", 3) == 0)
 			{
 				pPoint += 3;
 				continue;
 			}
 
-			// 5.純數字 [01] , 這在卍續藏出現
+			// 3.純數字 [01] , 這在卍續藏出現
 			if(*pPoint == u'[')
 			{
 				wchar_t * pTmp = pPoint;
@@ -270,7 +271,7 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 				pPoint = pTmp;
 			}
 
-			// 6.校勘數字 [<a id="a0001005" ....<..巢狀..>. >5</a>]
+			// 4.校勘數字 [<a id="a0001005" ....<..巢狀..>. >5</a>]
 			// ???? 這個新版的會改
 			if(*pPoint == u'[')
 			{
@@ -327,6 +328,25 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 				pPoint = pTmp;
 			}
 
+			// 5.忽略標點符號
+
+			if(CWordTool::GetWordType(*pPoint) == CWordTool::wtPunc)
+			{
+				if(*pPoint != u'[') // 組字式不忽略
+				{
+					pPoint++;
+					continue;
+				}
+			}
+
+			// 6.忽略空白
+
+			if(CWordTool::GetWordType(*pPoint) == CWordTool::wtSpace)
+			{
+				pPoint++;
+				continue;
+			}
+
 			// 表示都沒有要過濾的了
 			break;
 		}
@@ -336,7 +356,11 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 		// 傳回下一個字的長度, 可能是ext-b中文=2, 組字>2, 中英數符號1
 
 		int i1stWordLen;	// 第一個字的長度, 要記起來
-		int iThisWordLen = Get1PattenLen(pPoint);
+
+		// 待比對的字的長度
+		int iThisWordLen = CWordTool::GetFirstTokenLength(pPoint,true,true,true);
+		// 要查詢的字的長度
+		int iFindWordLen = CWordTool::GetFirstTokenLength(pFindWord,true,true,true);
 
 		if (*pFindWord == u'?')		// 萬用字元
 		{
@@ -344,10 +368,14 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 			// 這時 bMarching 一定是 true
 			pPoint += iThisWordLen;
 			pFindWord++;
-
 		}
-		else if(iPos>iAfterThisPos && wcsncmp(pPoint,pFindWord,iThisWordLen)==0)	// 找到
+		else if(iPos>iAfterThisPos &&
+				((wcsncmp(pPoint,pFindWord,iThisWordLen)==0 && iThisWordLen == iFindWordLen) ||
+				(iDesLen == iFindWordLen && wcsncmp(pDesPoint,pFindWord,iDesLen)==0)||
+				(iUniLen == iFindWordLen && wcsncmp(pUniPoint,pFindWord,iUniLen)==0 )))
+
 		{
+			// 找到了 (包括缺字中的組字式或unicode)
 			PosToReal[iPos] = make_pair(pPoint, iThisWordLen);
 
 			if(!bMatching)				// 目前是第一個字
@@ -359,117 +387,30 @@ void __fastcall CHighlight::GetOneFoundPos(int iNum)
 			}
 
 			pPoint += iThisWordLen;
-			pFindWord += iThisWordLen;
+			pFindWord += iFindWordLen;
+			pFindWord = NextFindPoint(pFindWord); 	// 移到下一個可以查詢的字
 
 			if(*pFindWord == 0)		// 比對完全結束, 恭禧!!!
 			{
 				bMatching = false;
 				FoundPos[iNum]->Add(iPosTmp,iPos);  // 將找到的位置存起來 (頭, 尾)
-				pFindWord = sWord.c_str();		// 要塗色的字
+				pFindWord = NextFindPoint(sWord.c_str());		// 要塗色的字
 			}
 		}
 		else	// 找不到
 		{
-            bool bFound = false;
-            // 沒找到, 但它可能是缺字, 所以要檢查後面是不是有 <!--gaiji,缽,1[金*本],2&#Xxxxx;,3-->
-			if(wcsncmp(pPoint+iThisWordLen,L"<!--gaiji,",10)==0)	// 後面是缺字
+			if(bMatching)
 			{
-				/* ???? 缺字待處理
-				wchar_t * pLast = pPoint+iThisWordLen;     // 要找最後結尾的指標
-                for(int i=0; i<500; i++)                // 最多 500 個字, 這不太可能了, 只是避免用 while 陷入無窮迴圈
-                {
-                    if(*pLast == '>') break;
-                    pLast++;
-                }
-                AnsiString sGaiji = AnsiString(pPoint+iThisWordLen , pLast +1 - pPoint-iThisWordLen);   // 取出 "<!--gaiji,缽,1[金*本],2&#Xxxxx;,3-->"
-                int iPos1 = sGaiji.AnsiPos(",");
-                int iPos2 = sGaiji.AnsiPos(",1");
-				int iPos3 = sGaiji.AnsiPos(",2");
-                int iPos4 = sGaiji.AnsiPos(",3");
-
-                AnsiString sNor = sGaiji.SubString(iPos1+1,iPos2-iPos1-1);    // 取出通用字
-                AnsiString sDes = sGaiji.SubString(iPos2+2,iPos3-iPos2-2);    // 取出組字式
-                AnsiString sUni = sGaiji.SubString(iPos3+2,iPos4-iPos3-2);    // 取出 unicode
-
-				if(sNor.Length()>0 && wcsncmp(sNor.c_str(),pFindWord,sNor.Length())==0)	// 和通用字相同
-                {
-                    if(!bMatching)				// 目前是第一個字
-        			{
-		        		i1stWordLen = iThisWordLen;
-				        bMatching = true;
-        				iPosTmp = iPos;			// 目前位置記起來
-        				pPointTmp = pPoint;		// 目前位置記起來
-        			}
-
-		        	pPoint += iThisWordLen;
-        			pFindWord = pFindWord + sNor.Length();
-
-			        if(*pFindWord == 0)		// 比對完全結束, 恭禧!!!
-        			{
-        				bMatching = false;
-        				FoundPos[iNum]->Add(iPosTmp,iPos);
-        				pFindWord = sWord.c_str();		// 要塗色的字
-        			}
-                    bFound = true;
-                }
-                if(sDes.Length()>0 && wcsncmp(sDes.c_str(),pFindWord,sDes.Length())==0)	// 和組字式相同
-                {
-                    if(!bMatching)				// 目前是第一個字
-        			{
-		        		i1stWordLen = iThisWordLen;
-				        bMatching = true;
-        				iPosTmp = iPos;			// 目前位置記起來
-        				pPointTmp = pPoint;		// 目前位置記起來
-        			}
-
-		        	pPoint += iThisWordLen;
-        			pFindWord = pFindWord + sDes.Length();
-
-			        if(*pFindWord == 0)		// 比對完全結束, 恭禧!!!
-        			{
-        				bMatching = false;
-        				FoundPos[iNum]->Add(iPosTmp,iPos);
-        				pFindWord = sWord.c_str();		// 要塗色的字
-        			}
-                    bFound = true;
-                }
-                if(sUni.Length()>0 && wcsncmp(sUni.c_str(),pFindWord,sUni.Length())==0)	// 和通用字相同
-                {
-                    if(!bMatching)				// 目前是第一個字
-        			{
-		        		i1stWordLen = iThisWordLen;
-				        bMatching = true;
-        				iPosTmp = iPos;			// 目前位置記起來
-        				pPointTmp = pPoint;		// 目前位置記起來
-        			}
-
-		        	pPoint += iThisWordLen;
-        			pFindWord = pFindWord + sUni.Length();
-
-			        if(*pFindWord == 0)		// 比對完全結束, 恭禧!!!
-        			{
-        				bMatching = false;
-						FoundPos[iNum]->Add(iPosTmp,iPos);
-        				pFindWord = sWord.c_str();		// 要塗色的字
-        			}
-                    bFound = true;
-				}
-				*/
-            }
-
-            if(!bFound)     // 依然沒找到, 連缺字都沒有符合
-            {
-			    if(bMatching)
-    			{
-    				bMatching = false;
-    				pPoint = pPointTmp;
-    				iPos = iPosTmp;
-    				pFindWord = sWord.c_str();		// 要塗色的字
-    				pPoint += i1stWordLen;
-    			}
-				else pPoint += iThisWordLen;
+				bMatching = false;
+				pPoint = pPointTmp;
+				iPos = iPosTmp;
+				pFindWord = NextFindPoint(sWord.c_str());		// 要塗色的字
+				pPoint += i1stWordLen;
 			}
+			else pPoint += iThisWordLen;
 		}
+		iDesLen = 0;    // 要歸零
+        iUniLen = 0;
 	}
 }
 // ---------------------------------------------------------------------------
@@ -599,268 +540,6 @@ void __fastcall CHighlight::KeepRangePos(void)
 				FoundPos[i]->Int2s->Delete(j);
 			}
 		}
-	}
-}
-//---------------------------------------------------------------------------
-// 找出此字串下一字的長度, 漢字英文數字為 1 , [金*本] 為 5, &#x1234; 為 8
-int __fastcall CHighlight::Get1PattenLen(const wchar_t * pData)
-{
-	if(*pData == 0) return 0;
-
-	if(*pData >= 0xD800 && *pData <= 0xDFFF)
-	{
-		return 2;	// ext-b 以上的中文字, 傳回 2
-	}
-	else if((*pData == u'&') && (*(pData+1) == u'#'))	// &#.....;
-	{
-		int iFind = 0;					// 找到 ; 的位置
-
-		for(int i=0; i<50; i++)		// &#x.....; 的長度應該不會超過 50
-		{
-			if(*(pData+i) == 0) break;
-			if(*(pData+i) == u';')
-			{
-				iFind = i+1;
-				break;
-			}
-		}
-
-		if(iFind > 0) return iFind;		// 果然是 &#.....;
-		else return 1;					// 找不到結尾, 所以傳回 1
-	}
-	else if(*pData == u'[')	// 可能是組字式了
-	{
-		int iFind = 0;					// 找到 ; 的位置
-
-		for(int i=1; i<100; i++)		// [?@((乏-之+虫)/((乏-之+虫)*(乏-之+虫)))] 的長度應該不會超過 100
-		{
-			if(*(pData+i) == 0) break;
-			else if(*(pData+i) == u'[')  // [ 裡面有 [ , 所以可能是這種修訂內有缺字 : [<span class="gaiji">&#X4F39;<!--gaiji,,1[仁-二+且],2&#X4F39;,3--></span>&gt;但]
-			{
-				iFind = 0;
-				break;
-			}
-			else if(*(pData+i) == u']')
-			{
-				iFind = i+1;
-				break;
-			}
-		}
-
-		if(iFind > 0) return iFind;		// 果然是組字式
-		else return 1;					// 找不到結尾, 所以傳回 1
-	}
-	else
-		return 1;		// 一般漢字、英文字或數字
-}
-// ---------------------------------------------------------------------------
-// 舊版, 不用了, 因為重疊的字無法順利塗色與連結
-// 實際塗色
-String __fastcall CHighlight::MakeHighlight_old()
-{
-/*
-	新的邏輯
-
-	1.開啟暫存空間
-	2.過濾標記及標記, 直到第一個字出現
-	3.判斷是不是下一個要變色的地方 (HighlightPos)
-	4.是就塗色
-*/
-
-	vector< wchar_t > vOutput;
-
-	HighlightPos.Begin = 0;		// highlight 的起點
-	HighlightPos.End = 0;		// highlight 的終點
-	HighlightPos.Num = 0;		// 第幾個詞
-	HighlightPos.Index = 0;		// 該詞的第幾次出現
-	HighlightPos.Next = 0 ;		// 下一個詞的編號, 通常是 Index + 1 , 若是最後一筆, 則是 0
-
-	wchar_t * pPoint = HTMLSource->FirstChar();		// 遊走在 HTMLSource 的指標
-
-	GetNextHighlightPos(true);	// 取得下一個要變色的詞的相關位置, 傳入true 是初值化
-
-	// 下一個要塗色的真實起始位置 (不忽略標記、標點)
-	pair<wchar_t *, int> prReal = PosToReal[HighlightPos.Begin];
-
-	while(*pPoint)
-	{
-		// 此詞要塗色
-		if(prReal.first == pPoint)
-		{
-			// 先放上連結的標籤 <a name="x_y"></a> // 第 x 詞, 第 y 次出現
-			AddSearchHeadName(&vOutput, HighlightPos.Num, HighlightPos.Index);
-
-			// 逐字處理
-			for(int i=HighlightPos.Begin; i<=HighlightPos.End; i++)
-			{
-				prReal = PosToReal[i];  // 目前此詞的下一個要塗色的字
-
-				// 如果此字前還有標記、標點, 就先略過去
-				while(pPoint < prReal.first)
-				{
-					vOutput.push_back(*pPoint);
-					pPoint++;
-                }
-
-				// <a href="#0_1" class="SearchWord1">宗</a>   // 第一詞第n個字連到第二次出現
-				// ....
-				// <a href="#1_1" class="SearchWord2">極</a>    // 第二詞第n個字連到第二次出現
-
-				// 處理此字
-				// 1.加上 <a name="x_y class="SearchWordx"> 標記
-				AddSearchNameTag(&vOutput, HighlightPos.Num, HighlightPos.Next);
-				// 2.加上此字
-				for(int len=0; len<prReal.second; len++)
-				{
-					vOutput.push_back(*pPoint);
-					pPoint++;
-				}
-				// 3.加上 </a>
-				vOutput.push_back(L'<');
-				vOutput.push_back(L'/');
-				vOutput.push_back(L'a');
-				vOutput.push_back(L'>');
-			}
-
-			GetNextHighlightPos();	// 取得下一個要變色的詞的相關位置
-			prReal = PosToReal[HighlightPos.Begin];
-		}
-		else
-		{
-			vOutput.push_back(*pPoint);
-			pPoint++;
-        }
-	}
-
-	return String(&vOutput[0],vOutput.size());
-}
-// ---------------------------------------------------------------------------
-// (舊版, 不用了, 因為重疊的字無法順利塗色與連結)
-	// 例如 : 要注意三種情況
-	// 1. 舍利 + 舍利子
-	// 這種情況是超過一組以上有同一個起點, 要塗最長的那一組比較好.
-	// 2. 雜阿含 + 阿含
-	// 這種情況是塗完第一組之後, 第二組的 x 雖然小於第一組, 但不能塗,
-	// 要確定第二組的 x 大於第一組的 y 才能塗.
-	// 3. 雜阿含 + 阿含經
-	// 如果此時經文是 "雜阿含經" , 塗完了 "雜阿含" , 第二組只能塗 "經" 了.
-
-	// ???? 沒有塗的詞, 上一個如何連到這一個詞?
-
-// 取得下一個要變色的詞的相關位置, 任何傳入值都表示初值化
-void __fastcall CHighlight::GetNextHighlightPos(bool bInitial)
-{
-/*
-	struct Smy_HighlightPos {
-		int Begin;	// highlight 的起點
-		int End;	// highlight 的終點
-		int Num;	// 第幾個詞
-		int Index;	// 該詞的第幾次出現
-		int Next;	// 下一個詞的編號, 通常是 Index + 1 , 若是最後一筆, 則是 0
-	} HighlightPos;
-*/
-	if(bInitial)	// 進行初值化
-	{
-		if(FoundPosIndex) delete[] FoundPosIndex;
-		FoundPosIndex = new int[SearchEngine->SearchWordList->Count];
-		for(int i=0; i<SearchEngine->SearchWordList->Count; i++)
-		{
-			*(FoundPosIndex+i) = 0;
-		}
-	}
-
-	int iMin = 99999999;
-
-	int iIndex = -1;
-
-	int iPtrY;  // 這是要暫存結尾那個字
-	// 例如查詢 (舍利 - 舍利子) + 舍利弗
-	// 為了避免 "舍利弗" 只塗色 "舍利" 二字, 所以要塗就塗最長的字串
-	// 因此要記錄 iPtrY
-	// ???? 實際請測試看看底下二者塗色是否 OK?
-	// (舍利 - 舍利子) + 舍利弗 vs 舍利弗 + (舍利 - 舍利子)
-
-	// ???? 可能最後一個 "舍利" 本來要塗在 "舍利弗" , 卻沒有塗, 造成最後一個 "舍利"
-	// 無法連回最前面, 這個再看能不能處理????
-
-	// 找出所有串列中最小的那一個.
-	for(int i=0; i<SearchEngine->SearchWordList->Count; i++)
-	{
-		if(*(FoundPosIndex+i) < FoundPos[i]->Int2s->Count)	// 還沒超過才能比較
-		{
-			// i 表示是第幾個詞
-			// *(FoundPosIndex+i) 是目前已使用到的索引, 由 0 開始
-			TPoint * tpPtr = (TPoint *) FoundPos[i]->Int2s->Items[*(FoundPosIndex+i)];
-			if(tpPtr->x < iMin)
-			{
-				iIndex = i;
-				iMin = tpPtr->x;
-				iPtrY = tpPtr->y;
-			}
-		}
-	}
-
-	if(iIndex != -1)		// 表示有找到
-	{
-		// 為了怕重覆, 所以每一個都調整
-		for(int i=0; i<SearchEngine->SearchWordList->Count; i++)
-		{
-			if(*(FoundPosIndex+i) < FoundPos[i]->Int2s->Count)	// 還沒超過才能比較
-			{
-				// i 表示是第幾個詞
-				// *(FoundPosIndex+i) 是目前已使用到的索引, 由 0 開始
-				TPoint * tpPtr = (TPoint *) FoundPos[i]->Int2s->Items[*(FoundPosIndex+i)];
-				if(tpPtr->x == iMin)
-				{
-					if(tpPtr->y >= iPtrY)	// 要比這個大才要記錄
-					{
-						iPtrY = tpPtr->y;
-						HighlightPos.Begin = tpPtr->x;
-						HighlightPos.End = tpPtr->y;
-						HighlightPos.Num = i;	// 第幾個詞
-						HighlightPos.Index = *(FoundPosIndex+i);	// 該詞的第幾次出現
-						HighlightPos.Next = HighlightPos.Index + 1; // 下一個詞的編號, 通常是 Index + 1 , 若是最後一筆, 則是 0
-						if(HighlightPos.Next == FoundPos[i]->Int2s->Count)	HighlightPos.Next = 0;
-					}
-					*(FoundPosIndex+i) = *(FoundPosIndex+i)+1;		// 該詞的索引退後一位
-				}
-			}
-		}
-	}
-	else
-	{
-		// 找不到了
-		HighlightPos.Begin = 0;
-		HighlightPos.End = 0;
-		HighlightPos.Num = 0;	// 第幾個詞
-		HighlightPos.Index = 0;	// 該詞的第幾次出現
-	}
-}
-// ---------------------------------------------------------------------------
-// 找到某個要塗色的詞, 加上 name 的標記讓人連結, 指出是第 iNum 詞第 iTime 次出現
-// <a name="iNum_iTime"></a>
-void __fastcall CHighlight::AddSearchHeadName(vector<wchar_t> * vOutput, int iNum, int iTime)
-{
-	String sTag = u"<a name=\"" + String(iNum) + u"_" + String(iTime) + u"\"></a>";
-	wchar_t * wc = sTag.FirstChar();
-	while(*wc)
-	{
-		vOutput->push_back(*wc);
-		wc++;
-	}
-}
-// ---------------------------------------------------------------------------
-// 加上 <a href="#x_y" class="SearchWordx"> 標記
-void __fastcall CHighlight::AddSearchNameTag(vector<wchar_t> * vOutput, int iNum, int iTime)
-{
-	int iMod = iNum % 5;	// 共有五種顏色, 輪著使用
-	String sTag = u"<a href=\"#" + String(iNum) + u"_" + String(iTime)
-		+ u"\" class=\"SearchWord" + String(iMod) + "\">";
-	wchar_t * wc = sTag.FirstChar();
-	while(*wc)
-	{
-		vOutput->push_back(*wc);
-		wc++;
 	}
 }
 // ---------------------------------------------------------------------------
@@ -1075,3 +754,78 @@ void __fastcall CHighlight::GetEveryWordInfo(void)
 	}
 }
 // ---------------------------------------------------------------------------
+// 移到下一個可以查詢的字
+// 例如查詢的字串是 "ABC XYA 如是，我聞"
+// 目前指標可能在空白或標點，要往下一個可查詢的字移到動
+wchar_t * CHighlight::NextFindPoint(wchar_t * pFindWord)
+{
+	while(1)
+	{
+		if(*pFindWord == 0) return pFindWord;   // 到底了
+		if(*pFindWord == u'?') return pFindWord;   // 問號是可以的
+
+		// 取出下一個字的長度
+		int iLen = CWordTool::GetFirstTokenLength(pFindWord,true,true,true);
+
+		if(iLen>1) return pFindWord;	// 長度 > 1 => ext-b, 組字, 標記, 英文單字 &xxx;
+
+		CWordTool::EWordType wtType = CWordTool::GetWordType(*pFindWord);
+
+		if(wtType == CWordTool::wtSpace || wtType == CWordTool::wtPunc)
+			pFindWord++;
+		else
+			return pFindWord;
+	}
+}
+// ---------------------------------------------------------------------------
+// 分析一個 <span class="gaiji"....> 標記
+void CHighlight::AnalysisGiajiTag(wchar_t ** pPoint, wchar_t ** pDesPoint, wchar_t ** pUniPoint, int * iDesLen, int * iUniLen)
+{
+	// <span class="gaiji" data-des="[組字式]" data-uni="xx" data-nor="xx" data-noruni="xx">
+
+	// 先取出整個 Tag
+
+	int iTagLen=1;
+	wchar_t * pTmp = *pPoint;
+	while(*pTmp != u'>')
+	{
+		iTagLen++;
+		pTmp++;
+	}
+	String sTag = String(*pPoint, iTagLen);
+	*pPoint = pTmp + 1;
+
+	int iPos;
+	// 先找有沒有組字式
+	iPos = sTag.Pos0(u"data-des");
+	*iDesLen = 0;
+	*pDesPoint = 0;
+	if(iPos >= 0)
+	{
+		*pDesPoint = sTag.FirstChar() + iPos + 10;	// 指到組字式的 [ 字
+		pTmp = *pDesPoint;
+		while(*pTmp != u'"')
+		{
+			*iDesLen = *iDesLen + 1;
+			pTmp++;
+		}
+	}
+	// 再找有沒有 Unicode
+	iPos = sTag.Pos0(u"data-uni");
+	*iUniLen = 0;
+	*pUniPoint = 0;
+	if(iPos >= 0)
+	{
+		*pUniPoint = sTag.FirstChar() + iPos + 10;	// 指到 Unicode
+		pTmp = *pUniPoint;
+		while(*pTmp != u'"')
+		{
+			*iUniLen = *iUniLen + 1;
+			pTmp++;
+		}
+	}
+}
+
+
+// ---------------------------------------------------------------------------
+
--- src/highlight.h
+++ src/highlight.h
@@ -17,16 +17,7 @@ private: // User declarations
 
 public: // User declarations
 
-	struct Smy_HighlightPos {
-		int Begin;	// highlight 的起點
-		int End;	// highlight 的終點
-		int Num;	// 第幾個詞
-		int Index;	// 該詞的第幾次出現
-		int Next;	// 下一個詞的編號, 通常是 Index + 1 , 若是最後一筆, 則是 0
-	} HighlightPos;
-
 	String * HTMLSource;                     // 來源字串
-	//vector< wchar_t > vText;            // 放置結果的
 
 	TmyPostfixStack * PostfixStack;		// 運算用的
 
@@ -55,8 +46,6 @@ public: // User declarations
 	// 每一個字的長度, 例如組字式就會比較長
 	map <wchar_t *, int> mpWordLength;
 
-	/////////////////////////////////////
-
 	// 每一個詞都有一個串列, 這是要記錄目前已畫線的串列是畫到什麼地方了.
 
 	int * FoundPosIndex;
@@ -69,16 +58,7 @@ public: // User declarations
 	void __fastcall FindTheRange(void);			// 將合乎要求的範圍找出來 (經過布林運算了), 傳回找到的組數
 	void __fastcall KeepRangePos(void);			// 將每一個詞符合最終位置的留下來.
 	void __fastcall GetEveryWordInfo(void);     // 把每一個字要定位、塗色與連結的資料都記錄下來, 包括重疊的字
-	void __fastcall GetNextHighlightPos(bool bInitial = false);	// 取得下一個要變色的詞的相關位置, 任何傳入值都表示初值化
-	int __fastcall Get1PattenLen(const wchar_t *);		// 找出此字串下一字的長度, 英文數字為 1 , [金*本] 為 7, &#x1234; 為 8
-	String __fastcall MakeHighlight_old();  			// 實際塗色
 	String __fastcall MakeHighlight();  			// 實際塗色
-
-	// 找到某個要塗色的詞, 加上 name 的標記讓人連結, 指出是第 iNum 詞第 iTime 次出現
-	// <a name="iNum_iTime"></a>
-	void __fastcall AddSearchHeadName(vector<wchar_t> * vOutput, int iNum, int iTime);
-	// 加上 <a href="#x_y" class="SearchWordx"> 標記
-	void __fastcall AddSearchNameTag(vector<wchar_t> * vOutput, int iNum, int iTime);
 
 	// 某個要塗色的詞, 加上定位 name 的標記讓人連結, 指出是第 iNum 詞第 iTime 次出現
 	void __fastcall AddWordAnchor(vector<wchar_t> * vOutput, wchar_t * pPoint);
@@ -86,6 +66,13 @@ public: // User declarations
 	// 加上連結及塗色的 class
 	void __fastcall AddWordLink(vector<wchar_t> * vOutput, wchar_t * pPoint);
 
+	// 移到下一個可以查詢的字
+	// 例如查詢的字串是 "ABC XYA 如是，我聞"
+	// 目前指標可能在空白或標點，要往下一個可查詢的字移到動
+	wchar_t * NextFindPoint(wchar_t * pFindWord);
+
+	// 分析一個 <span class="gaiji"....> 標記
+	void AnalysisGiajiTag(wchar_t ** pPoint, wchar_t ** pDesPoint, wchar_t ** pUniPoint, int * iDesLen, int * iUniLen);
 	__fastcall CHighlight(TmyMonster * seSearchEngine);
 	__fastcall ~CHighlight();
 };
